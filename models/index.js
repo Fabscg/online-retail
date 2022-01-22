ddebca9df1c5fea0d60bde8a9e2409f2d1ad02e0
Fabulosa <fabiscg79@gmail.com>
2022-01-22 13:16:15 -0500

    Added index.js information in the models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,32 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+//Create assosiations
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "categoy_id",
+  onDelete: "SET NULL"
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+  onDelete: "SET NULL"
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "product_tag",
+  foreignKey: "product_id",
+  onDelete: "SET NULL"
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: "ProductTag",
+  foreignKey: "tag_id",
+})
 
 module.exports = {
   Product,
