85f87e530c0ad5c4805896c6abe74b8ad456d5b9
Fabulosa <fabiscg79@gmail.com>
2022-01-21 22:33:44 -0500

    starting adding dependencies and seeds

--- Develop/server.js
+++ Develop/server.js
@@ -2,12 +2,28 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: false }));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(session(sess));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
